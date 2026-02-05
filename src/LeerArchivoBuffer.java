4aab147b809b78380af1b97df3f084273c35cd69
Judith-olmand <judith.olmand@educa.jcyl.es>
2026-02-05 11:01:54 +0100

    ACABADO

--- src/LeerArchivoBuffer.java
+++ src/LeerArchivoBuffer.java
@@ -1,13 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
-
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+public class LeerArchivoBuffer {
+    public static void main(String[] args) {
+        try (BufferedReader reader = new BufferedReader(new FileReader("../archivo.txt"))) {
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                System.out.println(linea);
+            }
+        } catch (IOException e) {
+            System.out.println("Ocurrió un error al leer el archivo: " + e.getMessage());
+        }
+    }
 }
